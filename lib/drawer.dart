e08f3f874a1d1652f86bb0c884e2174e58ece2be
safa724 <safanoushad724@gmail.com>
2024-07-23 10:23:29 +0530

    Updated Code

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -6,7 +6,6 @@ import 'package:ibeuty/userprofile.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AppDrawer extends StatefulWidget {
-  
   @override
   State<AppDrawer> createState() => _AppDrawerState();
 }
@@ -16,12 +15,13 @@ class _AppDrawerState extends State<AppDrawer> {
   String userName = '';
   String userEmail = '';
 
- @override
+  @override
   void initState() {
     super.initState();
     checkLoginStatus();
   }
-Future<void> checkLoginStatus() async {
+
+  Future<void> checkLoginStatus() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
       isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
@@ -31,116 +31,114 @@ Future<void> checkLoginStatus() async {
       }
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
-  child: ListView(
-    children: <Widget>[
-      Container(
-      color: Colors.white,
-        height: 170,
-        width: 150,
-        child: Padding(
-          padding: EdgeInsets.all(30),
-          child: Column(
-            
-            children: [
-          
-              Row(
+      child: ListView(
+        children: <Widget>[
+          Container(
+            color: Colors.white,
+            height: 170,
+            width: 150,
+            child: Padding(
+              padding: EdgeInsets.all(30),
+              child: Column(
                 children: [
-                 Container(
-                  height: 70,
-                  width: 70,
-                  color: Colors.white,
-                  child: Image.asset('images/person.png')
-                 ),
-                 SizedBox(
-                  width: 30,
-                 ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  Row(
                     children: [
-                      Text(
-                        userName,
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        
-                        ),
+                      Container(
+                          height: 70,
+                          width: 70,
+                          color: Colors.white,
+                          child: Image.asset('images/person.png')),
+                      SizedBox(
+                        width: 30,
                       ),
-                      Text(
-                        userEmail,
-                        style: TextStyle(
-                          fontSize: 10,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.grey,
-                        
-                        ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            userName,
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Text(
+                            userEmail,
+                            style: TextStyle(
+                              fontSize: 10,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
                 ],
               ),
-            ],
+            ),
           ),
-        ),
-      
-    
+          ListTile(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Home()),
+              );
+            },
+            leading: Icon(Icons.home_filled, color: Colors.grey),
+            title: Text(
+              "Home",
+              style: TextStyle(color: Colors.grey),
+            ),
+          ),
+          ListTile(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => UserProfileScreen()));
+            },
+            leading: Icon(Icons.person, color: Colors.grey),
+            title: Text(
+              "Profile",
+              style: TextStyle(color: Colors.grey),
+            ),
+          ),
+          ListTile(
+            onTap: () {
+              //       Navigator.push(
+              //   context,
+              //   MaterialPageRoute(builder: (context) => UserProfileScreen())
+              // );
+            },
+            leading: Icon(Icons.menu, color: Colors.grey),
+            title: Text(
+              "Orders",
+              style: TextStyle(color: Colors.grey),
+            ),
+          ),
+          ListTile(
+            onTap: () {
+              //       Navigator.push(
+              //   context,
+              //   MaterialPageRoute(builder: (context) => UserProfileScreen())
+              // );
+            },
+            leading: Icon(
+              Icons.favorite_outline,
+              color: Colors.grey,
+            ),
+            title: Text(
+              "My Wishlist",
+              style: TextStyle(color: Colors.grey),
+            ),
+          ),
         
+        ],
       ),
-     
-      ListTile(
-        onTap: () {
-          Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => Home()),
     );
-        },
-        leading: Icon(Icons.home_filled,color: Colors.grey),
-        title: Text("Home",style: TextStyle(color: Colors.grey),),
-      ),
-      ListTile(
-        onTap: () {
-           Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => UserProfileScreen())
-    );
-        },
-       leading: Icon(Icons.person,color: Colors.grey),
-        title: Text("Profile",style: TextStyle(color: Colors.grey),),
-      ),
-      ListTile(
-        onTap: () {
-    //       Navigator.push(
-    //   context,
-    //   MaterialPageRoute(builder: (context) => UserProfileScreen())
-    // );
-        },
-        leading: Icon(Icons.menu,color: Colors.grey),
-        title: Text("Orders",style: TextStyle(color: Colors.grey),),
-      ),
-       ListTile(
-        onTap: () {
-    //       Navigator.push(
-    //   context,
-    //   MaterialPageRoute(builder: (context) => UserProfileScreen())
-    // );
-        },
-        leading: Icon(Icons.favorite_outline,color: Colors.grey,),
-        title: Text("My Wishlist",style: TextStyle(color: Colors.grey),),
-      ),
-     ListTile(
-  onTap: () {
-    // Handle Logout functionality
-    exit(0); // Exit the application
-  },
-  leading: Icon(Icons.login_outlined,color:Colors.grey),
-  title: Text("Logout",style: TextStyle(color: Colors.grey),),
-),
-    ],
-  ),
-);
   }
 }
